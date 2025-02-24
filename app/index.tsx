6c5a26fe78888b856714fa9758c503cb356d3d2e
Pedro Selas <pedro.selas@gmail.com>
2025-02-24 13:19:53 +0100

    INIT - Changing Stack config and removing public folder + SW

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,18 @@
-import PostViews from "@/components/PostsView"
-export default function BloggersScreen() {
+import { Stack } from 'expo-router';
+import {ThemedView} from "@/components/ThemedView";
+import PostsView from "@/components/PostsView";
+
+export default function Home() {
+
+
     return (
-        <PostViews/>
-)}
+        <ThemedView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Stack.Screen
+                options={{
 
-
+                }}
+            />
+            <PostsView/>
+        </ThemedView>
+    );
+}
